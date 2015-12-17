c7f55bd284be3e2b1650aaf4c90520c6ae9232a0
Jianfeng Li <jianfeng.li@ersa.edu.au>
2015-12-18 09:52:37 +1030

    Recommend using template.cached.loader in production

--- app/prod_settings.py
+++ app/prod_settings.py
@@ -53,12 +53,16 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+           'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.app_directories.Loader',
+                ]),
             ],
         },
     },
